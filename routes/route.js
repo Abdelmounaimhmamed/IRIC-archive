37a8755be0936be09fa05acc7c1701aa9b7af907
Abdelmounaimhmamed <mounaimhmamed@gmail.com>
2022-09-11 15:42:19 +0100

    make it better

--- routes/route.js
+++ routes/route.js
@@ -1,40 +1,71 @@
 const express = require("express")
 const router = express.Router()
 const Contact = require("../models/contact")
-const data = [
+const data_s1 = [
     {
         id :1 ,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Architecture et interconnexion des réseaux",
     },
     {
         id : 2,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Traitement du signal & Modulation",
     },{
         id : 3,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Soft embarqué",
     }
     ,{
         id : 4,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Administration réseaux",
     }
     ,{
         id : 5,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Architecture des ordinateurs / Electronique numérique",
     },{
         id : 6,
         icon : "fa-solid fa-shield-halved",
-        title : "cyber punk",
+        title : "Economie du marché et gestion budgétaire",
+    }
+]
+const data_s2 = [
+    {
+        id :1 ,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
+    },
+    {
+        id : 2,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
+    },{
+        id : 3,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
+    }
+    ,{
+        id : 4,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
+    }
+    ,{
+        id : 5,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
+    },{
+        id : 6,
+        icon : "fa-solid fa-shield-halved",
+        title : "...",
     }
 ]
 
 
+
 router.get("/" , (req,res) => {
-    res.render("home.ejs" , {items : data})
+    res.render("home.ejs" , {items : data_s1 , items_2 : data_s2})
 })
 router.get("/courses" , (req ,res) => {
     res.render( "courses.ejs")

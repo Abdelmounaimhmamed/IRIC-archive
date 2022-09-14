9eef4504978ae634b3e78c589ef6eadd040e09b0
Abdelmounaimhmamed <mounaimhmamed@gmail.com>
2022-09-14 15:11:35 +0100

    make it better

--- routes/route.js
+++ routes/route.js
@@ -55,34 +55,127 @@ const data_s1 = [
         }
     }
 ]
+
 const data_s2 = [
     {
-        id :"1" ,
+        id :"7" ,
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "Embarque",
+        module_1: {
+            link : "https://drive.google.com/file/d/1yeYery2dj4Hzk7gPtgPjLY56Dfwn_1ok/view?usp=sharing",
+            title : "VHDL"
+        },
+        module_2 : {
+            title : "",
+            link : ""
+        },
+        module_3 : {
+            title : "",
+            link : ""
+        }, module_4 : {
+            title : "",
+            link : ""
+        }
     },
     {
-        id : "2",
+        id : "8",
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "filtrage numerique",
+        module_1 : {
+            title : "",
+            link : ""
+        },
+        module_2 : {
+            title : "",
+            link : ""
+        },
+        module_3 : {
+            title : "",
+            link : ""
+        }, module_4 : {
+            title : "",
+            link : ""
+        }
     },{
-        id : "3",
+        id : "9",
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "modelisation et ingenierie des trafics",
+        module_1 : {
+            title : "",
+            link : ""
+        },
+        module_2 : {
+            title : "",
+            link : ""
+        },
+        module_3 : {
+            title : "",
+            link : ""
+        }, module_4 : {
+            title : "",
+            link : ""
+        }
     }
     ,{
-        id : "4",
+        id : "10",
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "securite reseau",
+        module_1 : {
+            title : "chap_1_introduction",
+            link:"https://drive.google.com/file/d/1kx3rUr-kxHCw4XKl-IB1xFbuty09WSzI/view?usp=sharing"
+        },
+        module_2 : {
+            title : "chap_2_sniffing Réseau",
+            link : "https://docs.google.com/presentation/d/1UQxzJAB9Z4B-5JT9Pi5vI8pkSHNGTzA4/edit#slide=id.p1",
+        },
+        module_3 : {
+            title : "securite des echanges",
+            link : "https://docs.google.com/presentation/d/1dFhtpssoEyS4XNqCz86CDx_6y05Y6wrN/edit#slide=id.p1",
+        },
+        module_4 : {
+            title : "configuration de firewall",
+            link : "https://docs.google.com/presentation/d/1myGmQWHLzmWowIxjKnIbGNunQqGbXGWc/edit#slide=id.p1",
+        }
     }
     ,{
-        id : "5",
+        id : "11",
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "signaux aleatoires",
+        module_1 : {
+            title : "",
+            link : ""
+        },
+        module_2 : {
+            title : "",
+            link : ""
+        },
+        module_3 : {
+            title : "",
+            link : ""
+        }, module_4 : {
+            title : "",
+            link : ""
+        }
     },{
-        id : "6",
+        id : "12",
         icon : "fa-solid fa-shield-halved",
-        title : "...",
+        title : "theorie de l'info",
+        module_1 : {
+            title : "theo. chap_1",
+            link:"https://drive.google.com/file/d/1jPDirn2UUiRvbbyVhqX8P_b2hOxMITkr/view?usp=sharing",
+        },
+        module_2 : {
+            title : "theo. chap_2 ",
+            link : "https://drive.google.com/file/d/1qgCPqzl-0Z9JVsv62RS87W6CNf1URsnR/view?usp=sharing"
+        },
+        module_3 : {
+            title : "TD_1",
+            link : "https://drive.google.com/file/d/1q0WS9lTbBOZ3cyDkR5LOPXRVMJQDZI8_/view?usp=sharing",
+        },
+        module_4 : {
+            title : "TD_2",
+            link : "https://drive.google.com/file/d/1AuwZU2iYy7AykuJfFlTiVX8xmw1vUxng/view?usp=sharing",
+        }
     }
 ]
 
@@ -128,6 +221,30 @@ router.get("/courses/:id" , (req,res) => {
              })
             }
         })
+        data_s2.forEach((data) => {
+            if (id === data.id) {
+                console.log(data.title)
+              res.render("courses.ejs" , {
+                content : data.title,
+                 module_1 : {
+                    title : data.module_1.title,
+                    link : data.module_1.link
+                 },
+                 module_2 : {
+                    title : data.module_2.title,
+                    link : data.module_2.link
+                 },
+                 module_3 : {
+                    title : data.module_3.title,
+                    link : data.module_3.link
+                 },
+                 module_4 : {
+                    title : data.module_4.title,
+                    link : data.module_4.link
+                 }
+              })
+             }
+         })
         
    
 })
